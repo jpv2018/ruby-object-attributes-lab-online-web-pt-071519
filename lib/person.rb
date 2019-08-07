32019d54a9ea709d9abbdc77c97fbc10b9285215
Jason Viola <jason.viola@mavs.uta.edu>
2019-08-07 01:21:54 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(persons_name)
+    @this_person = persons_name
+  end
+  
+  def name(persons_name)
+    @this_person
+  end
+  
+  def job=(persons_job)
+    @this_job = persons_job
+  end
+  
+  def job(persons_job)
+    @this_job
+  end
+  
