1213fac7323102abac0a08d261e0daace89870ad
Jason Viola <jason.viola@mavs.uta.edu>
2019-08-07 01:20:02 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+  
+  def name(dog_name)
+    @this_dogs_name = dog_name
+  end
+  
+  def breed=(dog_breed)
+    @this_dogs_breed = dog_breed
+  end
+  
+  def breed(dog_breed)
+    @this_dogs_breed = dog_breed
+  end
