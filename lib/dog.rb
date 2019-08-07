ff44c8d03b527a20ec4beee762c418e7a9fa7623
Jason Viola <jason.viola@mavs.uta.edu>
2019-08-07 01:20:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,7 +4,7 @@ class Dog
   end
   
   def name(dog_name)
-    @this_dogs_name = dog_name
+    @this_dogs_name
   end
   
   def breed=(dog_breed)
@@ -12,5 +12,6 @@ class Dog
   end
   
   def breed(dog_breed)
-    @this_dogs_breed = dog_breed
+    @this_dogs_breed
   end
+end
