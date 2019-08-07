bdb4e7e15b5cf4ed5de04899301c3e501c2f3622
Jason Viola <jason.viola@mavs.uta.edu>
2019-08-07 01:37:19 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,18 @@
 class Person
   def name=(persons_name)
-    @this_person = persons_name
+    @name = persons_name
   end
   
   def name(persons_name)
-    @this_person
+    @name
   end
   
   def job=(persons_job)
-    @this_job = persons_job
+    @job = persons_job
   end
   
   def job(persons_job)
-    @this_job
+    @job
   end
 end
   
